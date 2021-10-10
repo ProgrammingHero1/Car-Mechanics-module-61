92e017b647b8f8a216bb5d65a0eda0a4d2fee604
Programming Hero <programming.hero1@gmail.com>
2021-10-10 17:38:30 -0500

    first commit

---
+++ src/Pages/Home/Service/Service.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Service.css';
+
+const Service = ({ service }) => {
+    // const {service} = props;
+    const { name, price, description, img } = service;
+    return (
+        <div className="service">
+            <img src={img} alt="" />
+            <h3>{name}</h3>
+            <h5>Price: {price}</h5>
+            <p>{description}</p>
+        </div>
+    );
+};
+
+export default Service;
